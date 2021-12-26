63c2fe78cb38b03deb56b97c3265ff6891881024
Eren Akel <eren.akel.46@gmail.com>
2021-12-26 16:50:36 +0300

    Login Screen style changed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,6 +11,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffF6F6F6),
       appBar: AppBar(
         backgroundColor: const Color(0xffF6F6F6),
         elevation: 0,
